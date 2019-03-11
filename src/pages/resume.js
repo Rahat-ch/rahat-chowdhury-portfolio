d346889382e9e4cb665a9467e8f4b1f976c44ab8
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-03-10 23:57:37 -0400

    resume in public

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,37 +1,13 @@
-import React, { Component } from "react";
-import { Document, Page } from "react-pdf";
+import React from "react";
 
-export default class App extends Component {
-    state = { numPages: null, pageNumber: 1 };
-
-    onDocumentLoadSuccess = ({ numPages }) => {
-        this.setState({ numPages });
-    };
-
-    goToPrevPage = () =>
-        this.setState(state => ({ pageNumber: state.pageNumber - 1 }));
-    goToNextPage = () =>
-        this.setState(state => ({ pageNumber: state.pageNumber + 1 }));
-
-    render() {
-        const { pageNumber, numPages } = this.state;
-
-        return (
-            <div>
-
-                <div style={{ width: 600, marginLeft: 500 }}>
-                    <Document
-                        file="/resume.pdf"
-                        onLoadSuccess={this.onDocumentLoadSuccess}
-                    >
-                        <Page pageNumber={pageNumber} width={600} />
-                    </Document>
-                </div>
-
-                <p>
-                    Page {pageNumber} of {numPages}
-                </p>
-            </div>
-        );
+class Resume extends React.Component{
+    render(){
+        return(
+            
+                <embed className="resume" src="/resume.pdf" type="application/pdf" />
+            
+        )
     }
 }
+
+export default Resume
