edbd4a156e4b789a0dd0ac76146a70e1afbd9e1d
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-03-10 23:51:23 -0400

    resume fix

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import { Document, Page } from "react-pdf";
 
-class Resume extends React.Component{
-    render(){
-        return(
-            
-                <iframe className="resume" src="/resume.pdf" type="application/pdf" />
-            
-        )
+export default class App extends Component {
+    state = { numPages: null, pageNumber: 1 };
+
+    onDocumentLoadSuccess = ({ numPages }) => {
+        this.setState({ numPages });
+    };
+
+    goToPrevPage = () =>
+        this.setState(state => ({ pageNumber: state.pageNumber - 1 }));
+    goToNextPage = () =>
+        this.setState(state => ({ pageNumber: state.pageNumber + 1 }));
+
+    render() {
+        const { pageNumber, numPages } = this.state;
+
+        return (
+            <div>
+
+                <div style={{ width: 600, marginLeft: 500 }}>
+                    <Document
+                        file="/resume.pdf"
+                        onLoadSuccess={this.onDocumentLoadSuccess}
+                    >
+                        <Page pageNumber={pageNumber} width={600} />
+                    </Document>
+                </div>
+
+                <p>
+                    Page {pageNumber} of {numPages}
+                </p>
+            </div>
+        );
     }
 }
-
-export default Resume
