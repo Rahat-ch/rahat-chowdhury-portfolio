72e144ad2f23fab210e98abe2f67006daaf063dd
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-03-11 21:13:33 -0400

    resume refactor

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React from 'react'
+import PDFResume from '../assets/resume.pdf'
 
-class Resume extends React.Component{
-    render(){
-        return(
-            
-                <embed className="resume" src="/resume.pdf" type="application/pdf" />
-            
+class Resume extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            height: null,
+            width: null,
+        }
+    }
+    componentDidMount() {
+        if (typeof window !== 'undefined') {
+            this.setState((state, props) => ({
+                height: window.innerHeight,
+                width: window.innerWidth,
+            }))
+        }
+    }
+    render() {
+        const { height, width } = this.state
+        const dimensions = height && width
+        return (
+            dimensions && (
+                <embed
+                    src={PDFResume}
+                    height={height}
+                    width={width}
+                    type="application/pdf"
+                />
+            )
         )
     }
 }
